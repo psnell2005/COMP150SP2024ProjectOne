bee7bf4d8a66af32965d38d0ec1f3d478f8484e7
Phoebe <156856539+psnell2005@users.noreply.github.com>
2024-03-22 19:30:35 +0000

    Fixed the order and updated the edits to match Miller's edits from class yesterday

--- project_code/main.py
+++ project_code/main.py
@@ -1,42 +1,73 @@
 # main.py
 import sys
+from typing import List 
+import json
 import random
-from enum import Enum
 from project_code.src.UserInputParser import UserInputParser
 from project_code.src.InstanceCreator import InstanceCreator
 from project_code.src.UserFactory import UserFactory
+
+class Location:
+    
+    def __init__(self, parser, number_of_events: int = 1): 
+        self.parser = parser
+        self.events = [Event(self.parser) for _in range(number_of_events)]
+
+    import json 
+
+    def create_custom_event_from_static_text_file(self, file_path):
+        with open(file_path, "r") as file:
+            data = json.load(file)
+
+        return Event(self.parser, data)
+    
+
+from enum import Enum
 
 class EventStatus(Enum):
      UNKNOWN = "unknown"
      PASS = "pass"
      FAIL = "fail"
-     PARTIAL = "partial pass"
+     PARTIAL_PASS = "partial pass"
 
-def start_game():
-    parser = UserInputParser()
-    user_factory = UserFactory()
-    instance_creator = InstanceCreator(user_factory, parser)
+class Event:
+    def __init__(self, parser, data: dict = None):
+                self.parser = parser
+                self.primary = data['primary_attribute']
+                self.secondary = data['secondary_attribute']
+                self.prompt_text = data['prompt text']
+                self.pass_ = data['pass']
+                self.fail = data['fail']
+                self.partial_pass = data['partial pass']
 
-    response = parser.parse("Would you like to start a new game? (yes/no)")
-    print(f"Response: {response}")
-    user = instance_creator.get_user_info(response)
-    if user is not None:
-        game_instance = user.current_game
-        if game_instance is not None:
-            response = game_instance.start_game()
-            if response == "Save and quit":
-                user.save_game()
-                print("Game saved. Goodbye!")
-                sys.exit()
-            elif response:
-                print("Goodbye!")
-                sys.exit()
-    else:
-        print("See you next time!")
-        sys.exit()
+                self.status = EventStatus.UNKNOWN
+                self.fail = {
+                    "message": "You failed."
+                }
+                self.pass_ = {
+                    "message": "You passed."
+                self.partial_pass = {
+                    "message": "You partially passed."
+                }
+                self.prompt_text = "A challenging coding problem appears, what will you do?"
+                self.primary: Statistic = Strength() 
+                self.secondary: Statistic = Dexterity() 
+                 
+    def execute(self, party):
+        chosen_one = self.parser.select_party_member(party)
+        chosen_skill = self.parser.select_skill(chosen_one)
+        self.set_status(EventStatus.PASS)
+        pass 
 
-if __name__ == '__main__':
-    start_game()
+    def set_status(self, status: EventStatus = EventStatus.UNKNOWN) 
+        self.status = status 
+    
+    def resolve_choice(self, party, character, chosen_skill):
+        # check if the skill attributes overlap with the event attributes 
+        # if they don't, the character fails 
+        # if they do overlap, character passes
+        # if one overlaps, the character partially passes 
+        # to do this: make some checks --> is attr in skill, etc.
 
 class Character:
 
@@ -70,96 +101,26 @@ class Character:
         return "Bob"
 
 character1 = Character(name="Miller")
-
-
 character2 = Character(name="James")
-
-
 character3 = Character(name="Jen")
- 
-
 character4 = Character(name="Phoebe")
-
-
 character5 = Character(name="Therese")
-
-
 character6 = Character(name="Larry")
-
-
 character7 = Character(name="Gabe")
-
-
 character8 = Character(name="Trinity")
-
-
 character9 = Character(name="Johnathan")
-
-
 character10 = Character(name="Arjun")
-
-
 character11 = Character(name="Milosz")
-
-
 character12 = Character(name="Hookah")
-
-
 character13 = Character(name="Sherron")
-
-
 character14 = Character(name="Abdullah")
- 
-
 character15 = Character(name="Abdul")
-
-
 character16 = Character(name="Alex C.")
-
-
 character17 = Character(name="Adriana") 
-
-
 character18 = Character(name="Alex P.")
-
-
 character19 = Character(name="Jay")
-
-
 character20 = Character(name="Luke")
 
-
-class Event:
-    def __init__(self, parser):
-                self.parser = parser
-                self.status = EventStatus.UNKNOWN
-                self.fail = {
-                    "message": "You failed."
-                }
-                self.pass = {
-                    "message": "You passed."
-                self.partial_pass = {
-                    "message": "You partially passed."
-                }
-                self.primary: Statistic = Strength() 
-                self.secondary: Statistic = Dexterity() 
-                 
-    def execute(self, party):
-        self.parser.select_party_member(party)
-        chosen_one = self.parser.select_party_member(party)
-        chosen_skill = self.parser.select_skill(chosen_one)
-        self.set_status(EventStatus.PASS)
-        pass 
-
-    def set_status(self, status: EventStatus = EventStatus.UNKNOWN) 
-        self.status = status 
-    
-    def resolve_choice(self, party, character, chosen_skill):
-        # check if the skill attributes overlap with the event attributes 
-        # if they don't, the character fails 
-        # if they do overlap, character passes
-        # if one overlaps, the character partially passes 
-        # to do this: make some checks --> is attr in skill, etc.
                  
 class Game:
     def __init__(self, parser):
@@ -170,8 +131,9 @@ class Game:
         self.party: List[Character] = []
         self.current_location = None
         self.current_event = None
-        self._initialize_game()
         self.continue_playing = True
+
+        self._initialize_game()
         
 
     def add_character(self, character: Character):
@@ -189,9 +151,13 @@ class Game:
     def _initialize_game(self):
         """Initialize the game with characters, locations, and events based on the user's properties."""
         character_list = [Character() for _in range(10)]
-        location_list = [Location() for _in range(2)]
+        location_list = [Location(self.parser) for _in range(2)]
 
         for character in character_list:
+            self.add_character(character)
+
+        for location in location list: 
+        self.add_location(location)
 
     def start_game(self):
         return self._main_game_loop()
@@ -208,15 +174,40 @@ class Game:
                 return "Save and quit"
             else:
                 continue 
-            if self.continue_playing is False:
-                return True
-            elif self.continue_playing == "Save and quit":
-                return "Save and quit"
-            else:
-                return False
-        
-class UserFactory:
+        if self.continue_playing is False:
+            return True
+        elif self.continue_playing == "Save and quit":
+            return "Save and quit"
+        else:
+            return False
 
+class User:
+
+    def __init__(self, parser, username: str, password: str, legacy_points: int = 0):
+        self.username = username
+        self.password = password
+        self.legacy_points = legacy_points
+        self.current_game = self._get_retrieve_saved_game_state_or_create_new_game()
+        self.parser = parser 
+
+    def _get_retrieve_saved_game_state_or_create_new_game(self) -> Game:
+        new_game = Game(self.parser)
+        return new_game
+
+    def save_game(self):
+        pass
+        
+class UserInputParser:
+
+    def __init__(self):
+        self.style = "console"
+
+    def parse(self, prompt) -> str:
+        response: str = input(prompt)
+        return response
+
+class UserFactory:
+    @staticmethod 
     def create_user(parser: UserInputParser) -> User:
         username = parser.parse("Enter a username: ")
         password = parser.parse("Enter a password: ")
@@ -235,7 +226,7 @@ class InstanceCreator:
         if "login" in response:
             return self._load_user()
         else:
-                return self.user_factory.create_user(self.parser)
+            return self.user_factory.create_user(self.parser)
 
     def get_user_info(self, response: str) -> User | None:
         if "yes" in response:
@@ -245,12 +236,6 @@ class InstanceCreator:
 
     def _load_user(self) -> User:
         pass
-
-class Location:
-    
-    def __init__(self, parser, number_of_events: int = 1): 
-        self.parser = parser
-        self.events = [Event(self.parser) for _in range(number_of_events)]
 
 
 class Statistic:
@@ -277,7 +262,7 @@ class Statistic:
         """Generate a starting value for the statistic based on random number and user properties."""
         """This is just a placeholder for now. Perhaps some statistics will be based on user properties, and others 
         will be random."""
-        return legacy_points % 100 + random.randint(1, 3)
+        return legacy_points / 100 + random.randint(1, 3)
 
 
 class Strength(Statistic):
@@ -344,8 +329,8 @@ class User:
         self.username = username
         self.password = password
         self.legacy_points = legacy_points
-        self.parser = parser 
         self.current_game = self._get_retrieve_saved_game_state_or_create_new_game()
+        self.parser = parser 
 
     def _get_retrieve_saved_game_state_or_create_new_game(self) -> Game:
         new_game = Game(self.parser)
@@ -354,12 +339,29 @@ class User:
     def save_game(self):
         pass
 
+def start_game():
+    parser = UserInputParser()
+    user_factory = UserFactory()
+    instance_creator = InstanceCreator(user_factory, parser)
 
-class UserInputParser:
+    response = parser.parse("Would you like to start a new game? (yes/no)")
+    print(f"Response: {response}")
+    user = instance_creator.get_user_info(response)
+    if user is not None:
+        game_instance = user.current_game
+        if game_instance is not None:
+            response = game_instance.start_game()
+            if response == "Save and quit":
+                user.save_game()
+                print("Game saved. Goodbye!")
+                sys.exit()
+            elif response:
+                print("Goodbye!")
+                sys.exit()
+    else:
+        print("See you next time!")
+        sys.exit()
 
-    def __init__(self):
-        self.style = "console"
-
-    def parse(self, prompt) -> str:
-        response: str = input(prompt)
-        return response
+if __name__ == '__main__':
+    start_game()
+        
