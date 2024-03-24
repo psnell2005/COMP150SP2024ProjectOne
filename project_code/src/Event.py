7b317db70787c56b8582edac17b577fe2e032848
Jennifer Roe <jennnroe@gmail.com>
2024-03-24 15:37:30 -0500

    Added events for attacks

---
+++ project_code/src/Event.py
@@ -0,0 +1,67 @@
+event1 = Event(
+    name="Embarking on a journey to defeat the Dragon",
+    requirements="Leave the main kingdom.",
+    success_outcome="Gives hope to the kingdom, making everyone proud.",
+    partial_success_outcome="Leaves the kingdom despite their bad reputation.",
+    failure_outcome="The hero's reputation is so bad that the kingdom doesn't allow them to leave."
+)
+
+event2 = Event(
+    name="Obtaining Legendary Weapons",
+    requirements="Embark on a journey to find and retrieve legendary weapons rumored to be capable of defeating the Dragon.",
+    success_outcome="The heroes locate and acquire powerful artifacts that enhance their abilities and increase their chances of victory.",
+    partial_success_outcome="The heroes find some legendary weapons, but not all of them. They must make do with what they have.",
+    failure_outcome="The heroes are unable to find any legendary weapons, leaving them ill-prepared for the final battle against the Dragon."
+)
+
+event3 = Event(
+    name="Confrontation with the Dragon",
+    requirements="Engage in a climactic battle with the Dragon.",
+    success_outcome="Through courage, teamwork, and determination, the heroes emerge victorious, vanquishing the Dragon and saving the world from his tyranny.",
+    partial_success_outcome="The heroes put up a valiant fight, but the Dragon proves too powerful to defeat outright. They manage to weaken him, buying time for a temporary retreat.",
+    failure_outcome="Despite their best efforts, the heroes are no match for the overwhelming might of the Dragon. They are defeated, and the world falls under his control."
+)
+
+event4 = Event(
+    name="Arjun attacks the Dragon",
+    requirements="Engage in battle with the dragon",
+    success_outcome="Arjun damages the dragon, weakening it significantly!",
+    partial_success_outcome="Arjun wounds the dragon" ,
+    failure_outcome="Arjun's attack doesn't work!"
+)
+
+event5 = Event(
+    name="Larry attacks the Dragon",
+    requirements="Engage in battle with the dragon",
+    success_outcome="Larry damages the dragon, weakening it significantly",
+    partial_success_outcome="Larry wounds the dragon",
+    failure_outcome="Larry's attack doesn't work!"
+)
+
+event6 = Event(
+    name="Therese attacks the Dragon",
+    requirements="Engage in battle with the dragon",
+    success_outcome="Therese damages the dragon, weakening it significantly!",
+    partial_success_outcome="Therese wounds the dragon",
+    failure_outcome="Therese's attack doesn't work!"
+)
+
+event7 = Event(
+    name="Jen attacks the Dragon",
+    requirements="Engage in battle with the dragon",
+    success_outcome="Jen damages the dragon, weakening it significantly!",
+    partial_success_outcome="Jen wounds the dragon",
+    failure_outcome="Jen's attack doesn't work!"
+)
+
+event8 = Event(
+    name="Dragon attacks",
+    requirements="Engage in battle with the heros",
+    success_outcome="Dragon deals great damage to the heros.",
+    partial_success_outcome="Dragon damagaes some of the heros",
+    failure_outcome="The heros dodge the attack!"
+)
+
+event9 = Event(
+    name=""
+)
